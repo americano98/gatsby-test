05cab4eb929c6db3d117de6b9e0d6cc86da4e100
americano98 <americano_98_000@mail.ru>
2021-10-29 03:12:31 +0300

    test commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const page3 = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>My first Gatsby page</h1>
+    <p>Creating web-sites with Gatsby is really fan</p>
+    <StaticImage
+      src="../images/ametist-1.jpg"
+      width={399}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Аметист"
+    />
+    <p>
+      <Link to="/">Go to home page</Link> <br />
+    </p>
+  </Layout>
+)
+
+export default page3
