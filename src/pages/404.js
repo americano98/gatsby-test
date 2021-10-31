32be000fa372301d3d37b2e1b3c7f74b4f690c3c
americano98 <americano_98_000@mail.ru>
2021-10-31 23:16:53 +0300

    Change the not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import * as React from "react"
 
 import Layout from "../components/layout"
@@ -7,7 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Go to <Link to="/">home page</Link></p>
   </Layout>
 )
 
